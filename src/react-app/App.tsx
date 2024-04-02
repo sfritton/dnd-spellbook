9ccc95bc2fabf50d8f28d69feb4d996c999d50a8
Sam Fritton <sfritton94@gmail.com>
2024-04-02 13:56:34 -0500

    removing react-query

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,11 +1,1 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { QueryTest } from './QueryTest';
-
-const queryClient = new QueryClient();
-
-export const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <h1>Howdy</h1>
-    {/* <QueryTest /> */}
-  </QueryClientProvider>
-);
+export const App = () => <h1>Howdy</h1>;
--- src/react-app/QueryTest.tsx
+++
@@ -1,7 +0,0 @@
-import { useQueryClassSpells } from './queries/use-query-class-spells';
-
-export const QueryTest = () => {
-  const result = useQueryClassSpells({ selectedClass: 'paladin' });
-
-  console.log(result.data?.data.spells.sort((a, b) => a.index?.localeCompare(b.index)));
-};
