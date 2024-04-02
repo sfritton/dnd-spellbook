370a0477a66a090750f89817420f4513b3d322d5
Sam Fritton <sfritton94@gmail.com>
2024-04-01 21:49:43 -0500

    yo so many spellz

--- src/fetch-spells/fetch-spell-details.ts
+++ src/fetch-spells/fetch-spell-details.ts
@@ -1,17 +1,41 @@
+import { readdir } from 'fs/promises';
 import * as SPELL_LISTS from '../../spells/spell-lists.json';
-import { rateLimitedMap } from './rate-limited-map';
+import { asyncMap, executeThenWait, rateLimitedMap } from './rate-limited-map';
 import { saveSpell } from './save-spell';
 import { consoleLogEmphasis } from './util';
+
+export const getSavedSpells = async () => {
+  const spellDirectories = await readdir(`${__dirname}/../../../spells`);
+
+  const allSpells = await Promise.all(
+    spellDirectories.map((directory) =>
+      /\.json$/.test(directory)
+        ? ([] as string[])
+        : readdir(`${__dirname}/../../../spells/${directory}`),
+    ),
+  );
+
+  return allSpells
+    .flat()
+    .reduce<Record<string, true>>(
+      (histogram, spell) => ({ ...histogram, [spell.replace(/\.json$/, '')]: true }),
+      {},
+    );
+};
 
 export const fetchSpellDetails = async () => {
   consoleLogEmphasis('Fetching Spell Details');
   const spellLists = Object.entries(SPELL_LISTS);
+  const savedSpells = await getSavedSpells();
 
-  await rateLimitedMap(spellLists.slice(0, 2), ([className, spellList], i) => {
+  await rateLimitedMap(spellLists, ([className, spellList], i) => {
     console.log(i, new Date(), `Fetching ${className} spells ...`);
-    return rateLimitedMap(spellList.slice(0, 2), (spell, j) => {
+    return asyncMap(spellList, (spell, j) => {
+      if (savedSpells[spell.name]) {
+        console.log(`\t${i}-${j}`, new Date(), `Found saved version of ${spell.name}, skipping`);
+      }
       console.log(`\t${i}-${j}`, new Date(), `Fetching and saving ${spell.name} ...`);
-      return saveSpell(spell.url, className);
+      return executeThenWait(() => saveSpell(spell.url, className));
     });
   });
 
--- src/fetch-spells/index.ts
+++ src/fetch-spells/index.ts
@@ -1,6 +1,8 @@
 import { fetchClassSpellLists } from './fetch-class-spells';
-import { fetchSpellDetails } from './fetch-spell-details';
+import { fetchSpellDetails, getSavedSpells } from './fetch-spell-details';
 
 // fetchClassSpellLists();
 
-fetchSpellDetails();
+// fetchSpellDetails();
+
+getSavedSpells();
