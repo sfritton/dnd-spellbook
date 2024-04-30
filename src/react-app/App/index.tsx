7760e86ed53d0dd8f0ef957f6359af8a7499c4ed
Sam Fritton <sfritton94@gmail.com>
2024-04-29 20:02:48 -0500

    bookmarks

--- src/react-app/App/index.tsx
+++ src/react-app/App/index.tsx
@@ -1,11 +1,9 @@
 import { Header } from '../Header';
-import { Sidebar } from '../Sidebar';
 import { Spellbook } from '../Spellbook';
 
 export const App = () => {
   return (
     <>
-      {/* <Sidebar /> */}
       <Header />
       <Spellbook />
     </>
