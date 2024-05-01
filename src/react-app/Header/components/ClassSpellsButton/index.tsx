f25235db367e56b3e366614460bd77f193e9c86a
Sam Fritton <sfritton94@gmail.com>
2024-04-30 21:15:45 -0500

    fixing drawer issue

--- src/react-app/Header/components/ClassSpellsButton/index.tsx
+++ src/react-app/Header/components/ClassSpellsButton/index.tsx
@@ -5,7 +5,7 @@ import * as spellLists from '../../../spells';
 import { Spell } from '../../../types';
 import styles from './index.module.css';
 import { useSpellListContext } from '../../../SpellListContext';
-import { useDialog } from '../../../Dialog';
+import { useDialog, useSingleDialog } from '../../../Dialog';
 
 const LEVEL_OPTIONS = [...new Array(10)].map((_, i) => ({
   value: i,
@@ -14,7 +14,7 @@ const LEVEL_OPTIONS = [...new Array(10)].map((_, i) => ({
 
 export const ClassSpellsButton = () => {
   const { appendSpells } = useSpellListContext();
-  const [ClassSpellsDrawer, { open, close }] = useDialog();
+  const { open, close } = useSingleDialog();
 
   const handleSubmit = useCallback(
     (e: FormEvent<HTMLFormElement>) => {
@@ -34,33 +34,38 @@ export const ClassSpellsButton = () => {
     [appendSpells, close],
   );
 
-  return (
-    <>
-      <button onClick={open}>Add from class list</button>
-      <ClassSpellsDrawer isDrawer title="Add spells from class list">
-        <form className={styles.classSpellsForm} onSubmit={handleSubmit}>
-          <label className={styles.selectLabel} htmlFor="class-select">
-            Choose a class
-          </label>
-          <select id="class-select">
-            {CLASSES.map(({ id, name }) => (
-              <option key={id} value={id}>
-                {name}
-              </option>
-            ))}
-          </select>
-          <fieldset>
-            <legend>Choose spell levels</legend>
-            {LEVEL_OPTIONS.map(({ value, label }) => (
-              <div key={value}>
-                <input type="checkbox" value={value} id={`level-${value}`} />
-                <label htmlFor={`level-${value}`}>{label}</label>
-              </div>
-            ))}
-          </fieldset>
-          <button type="submit">Load spells</button>
-        </form>
-      </ClassSpellsDrawer>
-    </>
-  );
+  const openDialog = useCallback(() => {
+    open({
+      title: 'Add spells from class list',
+      isDrawer: true,
+      children: (
+        <>
+          <form className={styles.classSpellsForm} onSubmit={handleSubmit}>
+            <label className={styles.selectLabel} htmlFor="class-select">
+              Choose a class
+            </label>
+            <select id="class-select">
+              {CLASSES.map(({ id, name }) => (
+                <option key={id} value={id}>
+                  {name}
+                </option>
+              ))}
+            </select>
+            <fieldset>
+              <legend>Choose spell levels</legend>
+              {LEVEL_OPTIONS.map(({ value, label }) => (
+                <div key={value}>
+                  <input type="checkbox" value={value} id={`level-${value}`} />
+                  <label htmlFor={`level-${value}`}>{label}</label>
+                </div>
+              ))}
+            </fieldset>
+            <button type="submit">Load spells</button>
+          </form>
+        </>
+      ),
+    });
+  }, [handleSubmit, open]);
+
+  return <button onClick={openDialog}>Add from class list</button>;
 };
--- src/react-app/Header/components/TypeaheadButton/index.tsx
+++ src/react-app/Header/components/TypeaheadButton/index.tsx
@@ -1,77 +1,19 @@
-import { useState } from 'react';
-import allSpells from '../../../../constants/spells/all.json';
-import { Spell } from '../../../types';
+import { useCallback } from 'react';
 import styles from './index.module.css';
-import { SpellSummary } from '../../../SpellSummary';
-import { useDialog } from '../../../Dialog';
-import { useSpellListContext } from '../../../SpellListContext';
-
-const allSpellsFlat = allSpells.flat();
+import { useSingleDialog } from '../../../Dialog';
+import { TypeaheadDrawerContent } from './TypeaheadDrawerContent';
 
 export const TypeaheadButton = () => {
-  const [value, setValue] = useState<string>('');
-  const [spells, setSpells] = useState<Spell.Summary[]>([]);
-  const [TypeaheadDrawer, { open, close }] = useDialog();
-  const { appendSpells } = useSpellListContext();
+  const { open, close } = useSingleDialog();
 
-  const makeHandleChange = (spell: Spell.Summary) => (isChecked: boolean) => {
-    if (isChecked) {
-      setSpells((prevSpells) => [...prevSpells, spell]);
-    } else {
-      setSpells((prevSpells) => prevSpells.filter(({ id }) => id !== spell.id));
-    }
-  };
+  const openDialog = useCallback(() => {
+    open({
+      title: 'Add spells by name',
+      isDrawer: true,
+      className: styles.typeaheadDrawer,
+      children: <TypeaheadDrawerContent close={close} />,
+    });
+  }, [open, close]);
 
-  return (
-    <>
-      <button onClick={open}>Add by name</button>
-      <TypeaheadDrawer className={styles.typeaheadDrawer} isDrawer title="Add spells by name">
-        <form autoComplete="off">
-          <input
-            autoComplete="false"
-            aria-hidden="true"
-            name="hidden"
-            type="text"
-            style={{ display: 'none' }}
-          />
-          <label htmlFor="spell-typeahead" className="hidden">
-            Add a spell by name
-          </label>
-          <input
-            type="text"
-            id="spell-typeahead"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          />
-        </form>
-        <ul>
-          {value
-            ? allSpellsFlat
-                .filter(({ title }) => title.match(new RegExp(value, 'i')))
-                .slice(0, 10)
-                .map((spell) => (
-                  <SpellSummary
-                    key={spell.id}
-                    {...spell}
-                    isChecked={Boolean(spells.find(({ id }) => id === spell.id))}
-                    onChange={makeHandleChange(spell)}
-                    showLevel
-                  />
-                ))
-            : null}
-        </ul>
-        {spells.length > 0 ? (
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              appendSpells(spells);
-              close();
-            }}
-          >
-            Add selected spells
-          </button>
-        ) : null}
-      </TypeaheadDrawer>
-    </>
-  );
+  return <button onClick={openDialog}>Add by name</button>;
 };
